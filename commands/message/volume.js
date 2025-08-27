f11c03bdd6c8e26eca3d17e6912fbed0f2c400cf
billywongyya <106290577+billywongyya@users.noreply.github.com>
2025-08-27 12:03:06 +0800

    Update volume.js

--- commands/message/volume.js
+++ commands/message/volume.js
@@ -20,16 +20,14 @@ module.exports = {
         message.shivaValidated = true;
         message.securityToken = COMMAND_SECURITY_TOKEN;
 
-        setTimeout(() => {
-            message.delete().catch(() => {});
-        }, 4000);
+        
         
         const volume = parseInt(args[0]);
         
         if (!volume || volume < 1 || volume > 100) {
             const embed = new EmbedBuilder().setDescription('❌ Please provide a valid volume level (1-100)! Example: `!volume 50`');
             return message.reply({ embeds: [embed] })
-                .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                ;
         }
 
         const ConditionChecker = require('../../utils/checks');
@@ -45,13 +43,13 @@ module.exports = {
             if (!conditions.hasActivePlayer) {
                 const embed = new EmbedBuilder().setDescription('❌ No music is currently playing!');
                 return message.reply({ embeds: [embed] })
-                    .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                    ;
             }
 
             if (!conditions.sameVoiceChannel) {
                 const embed = new EmbedBuilder().setDescription('❌ You need to be in the same voice channel as the bot!');
                 return message.reply({ embeds: [embed] })
-                    .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                    ;
             }
 
             const player = conditions.player;
@@ -59,13 +57,14 @@ module.exports = {
 
             const embed = new EmbedBuilder().setDescription(`🔊 Volume set to **${volume}%**`);
             return message.reply({ embeds: [embed] })
-                .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                ;
 
         } catch (error) {
             console.error('Volume command error:', error);
             const embed = new EmbedBuilder().setDescription('❌ An error occurred while setting volume!');
             return message.reply({ embeds: [embed] })
-                .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                ;
         }
     }
 };
+
