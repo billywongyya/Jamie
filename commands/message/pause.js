5445aff317d9daa873df03e581c360a0c62970e2
billywongyya <106290577+billywongyya@users.noreply.github.com>
2025-08-27 11:51:34 +0800

    Update pause.js

--- commands/message/pause.js
+++ commands/message/pause.js
@@ -20,9 +20,7 @@ module.exports = {
         message.shivaValidated = true;
         message.securityToken = COMMAND_SECURITY_TOKEN;
 
-        setTimeout(() => {
-            message.delete().catch(() => {});
-        }, 4000);
+        
         
         const ConditionChecker = require('../../utils/checks');
         const checker = new ConditionChecker(client);
@@ -38,7 +36,7 @@ module.exports = {
             if (errorMsg) {
                 const embed = new EmbedBuilder().setDescription(errorMsg);
                 return message.reply({ embeds: [embed] })
-                    .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                    ;
             }
 
             const player = conditions.player;
@@ -46,19 +44,20 @@ module.exports = {
             if (player.paused) {
                 const embed = new EmbedBuilder().setDescription('❌ Music is already paused!');
                 return message.reply({ embeds: [embed] })
-                    .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                    ;
             }
 
             player.pause(true);
             const embed = new EmbedBuilder().setDescription('⏸️ Music paused!');
             return message.reply({ embeds: [embed] })
-                .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                ;
 
         } catch (error) {
             console.error('Pause command error:', error);
             const embed = new EmbedBuilder().setDescription('❌ An error occurred while pausing music!');
             return message.reply({ embeds: [embed] })
-                .then(m => setTimeout(() => m.delete().catch(() => {}), 3000));
+                ;
         }
     }
 };
+
